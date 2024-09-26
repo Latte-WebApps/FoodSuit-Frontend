f227f404a8994969e654b84c55246cfb947f7f74
pachieeee <sebaspacheco2003@gmail.com>
2024-09-26 11:43:11 -0500

    fix: readjusted main to adapt to our product design.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,21 @@ import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
+import Aura from '@primevue/themes/aura'
 
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+    ripple: true,
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+
+        }
+    }
+})
+
+app.mount('#app')
