da228b47c7ac7adc1f25c531347ce07dcfa32745
pachieeee <sebaspacheco2003@gmail.com>
2024-09-25 19:03:17 -0500

    feat: modified main.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+
 
 createApp(App).mount('#app')
