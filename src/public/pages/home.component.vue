4a89a22edcce80a749fb87b479456fca58a5dd63
JeanGrandez <jpmle10@hotmail.com>
2024-09-27 05:02:10 -0500

    chore: home and home-report component creation

---
+++ src/public/pages/home.component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header>
+      <h1>Home</h1>
+    </header>
+
+    <home-report />
+  </div>
+</template>
+
+<script>
+// Ruta relativa
+import HomeReport from '../../reports/components/home-report.component.vue'; // Ruta relativa
+
+export default {
+  components: {
+    HomeReport
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #f8f9fa;
+  padding: 10px;
+}
+</style>
