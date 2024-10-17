7c170a491fbcf0e45d4eed760ff50349a3aecaf5
pachieeee <sebaspacheco2003@gmail.com>
2024-10-17 14:45:17 -0500

    feat: added date filter component.

---
+++ src/finance/components/date-filter.component.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "date-filter",
+  data() {
+    return {
+      date: new Date()
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card flex justify-center">
+    <pv-date-picker v-model="date" inline showWeek/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
