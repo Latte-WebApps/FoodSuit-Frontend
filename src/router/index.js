ebe4dac6ab5c33d06bb44b6d7c85bd3ca574bb43
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2024-10-19 22:42:31 -0500

    refactor: deleted unused imports

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,8 @@ import HomeReportComponent from "../reports/pages/home-report.component.vue";
 import FinanceOverviewComponent from "../finance/pages/finance-overview.component.vue";
 import InventoryComponent from "../inventory/pages/inventory.component.vue";
 import EmployeeComponent from "../schedules/pages/employee-component.vue";
-import AttendanceComponent from "../attendance/pages/attendance.component.vue";
-import AttendanceOverviewComponent from "../attendance/components/attendance-overview.component.vue";
-import employeePaymentsPageComponent from "../attendance/pages/employee-payments-page.component.vue";
-import EmployeePaymentsPageComponent from "../attendance/pages/employee-payments-page.component.vue";
+import ProductivityPageComponent from "../attendance/pages/productivity-page.component.vue";
+import AttendancePageComponent from "../attendance/pages/attendance-page.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -15,8 +13,8 @@ const router = createRouter({
         { path: '/finance', name: 'finance', component: FinanceOverviewComponent, meta: { title: 'Finance'} },
         { path: '/inventory', name: 'inventory', component: InventoryComponent, meta: { title: 'Inventory'} },
         { path: '/employee', name: 'employee', component: EmployeeComponent, meta: { title: 'Employee'}},
-        { path: '/attendance', name: 'attendance', component: AttendanceOverviewComponent, meta: { title: 'Attendance'}},
-        { path: '/productivity', name: 'productivity', component: EmployeePaymentsPageComponent, meta: { title: 'Productivity'}},
+        { path: '/attendance', name: 'attendance', component: AttendancePageComponent, meta: { title: 'Attendance'}},
+        { path: '/productivity', name: 'productivity', component: ProductivityPageComponent, meta: { title: 'Productivity'}},
         { path: '/', redirect: '/home' }
     ]
 })
