fec7936d0f3a548def88105ef269c8589451ad62
P1ero0VL <u20211a620@upc.edu.pe>
2024-11-19 23:18:57 -0500

    feat: updated dish entity to contain products

--- src/Orders/model/dish.entity.js
+++ src/Orders/model/dish.entity.js
@@ -1,8 +1,9 @@
 export class Dish {
-    constructor({id = null, name = '', price = '', category = ''}) {
+    constructor({id = null, name = '', price = '', category = '', products = []}) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.category = category;
+        this.products = [];
     }
 }
