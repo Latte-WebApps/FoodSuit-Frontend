a64e4f871d849498e557fe6845a2372fadc3035a
Pachieeee <sebaspacheco2003@gmail.com>
2024-11-20 03:46:21 -0500

    fix: removed most/least ordered dishes.

--- src/finance/pages/finance-overview.component.vue
+++ src/finance/pages/finance-overview.component.vue
@@ -22,29 +22,13 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-row gap-8 align-content-center">
+  <div class="flex flex-row gap-8 mt-8 align-content-center justify-content-center">
     <div class="flex align-items-center">
       <report-datatable :reports="reports"/>
     </div>
 
     <div class="align-items-center">
       <report-pie :reports="reports"/>
-      <div class="flex flex-row lg:gap-8 justify-content-center">
-        <pv-card class="p-3 w-9rem">
-          <template #header>
-            <p>Most ordered dish</p>
-            <img src="https://comedera.com/wp-content/uploads/sites/9/2022/03/Tacu-tacu-shutterstock_1604013508.jpg" alt="Most trending dish" width="120px"/>
-          </template>
-          <template #title>Tacu tacu</template>
-        </pv-card>
-        <pv-card class="p-3 w-9rem">
-          <template #header>
-            <p>Least ordered dish</p>
-            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FgAydtOLW2F8%2Fmaxresdefault.jpg" alt="Least trending dish" width="120px"/>
-          </template>
-          <template #title>Saltado de coliflor</template>
-        </pv-card>
-      </div>
 
     </div>
 
